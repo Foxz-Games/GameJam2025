860d2dfb83c03669006ba3744d398d08ab3c004a
Micah Weatherhead <micah_weatherhead@byu.edu>
2025-11-07 13:05:57 -0700

    adjust size of sprites; begin wall checks

--- assets/Scripts/PlayerMovement.cs
+++ assets/Scripts/PlayerMovement.cs
@@ -1,6 +1,8 @@
+using System.Numerics;
 using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using Vector2 = UnityEngine.Vector2;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -18,6 +20,16 @@ public class PlayerMovement : MonoBehaviour
 	public Transform groundCheckPos;
 	public Vector2 groundCheckSize = new Vector2(0.5f, 0.05f);
 	public LayerMask groundLayer;
+	
+	[Header("WallCheck")]
+	public Transform wallCheckPos;
+	public Vector2 wallCheckSize = new Vector2(0.5f, 0.05f);
+	public LayerMask wallLayer;
+
+	[Header("Gravity")]
+	public float baseGravity = 2f;
+	public float maxFallSpeed = 10f;
+	public float fallSpeedMultiplier = 2f;
 
 	void Start()
 	{
@@ -29,12 +41,22 @@ public class PlayerMovement : MonoBehaviour
     {
         rb.linearVelocity = new Vector2(horizontalMovement * moveSpeed, rb.linearVelocity.y);
 		GroundCheck();
+		Gravity();
     }
 
-    public void Move(InputAction.CallbackContext context)
-    {
-        horizontalMovement = context.ReadValue<Vector2>().x;
-    }
+	public void Move(InputAction.CallbackContext context)
+	{
+		horizontalMovement = context.ReadValue<Vector2>().x;
+	}
+	
+	private void Gravity()
+	{
+		if (rb.linearVelocity.y < 0)
+		{
+			rb.gravityScale = baseGravity * fallSpeedMultiplier;
+			rb.linearVelocity = new Vector2(rb.linearVelocity.x, Mathf.Max(rb.linearVelocity.y, -maxFallSpeed));
+		}
+	}
 
 	public void Jump(InputAction.CallbackContext context)
 	{
@@ -65,5 +87,7 @@ public class PlayerMovement : MonoBehaviour
 	{
 		Gizmos.color = Color.white;
 		Gizmos.DrawWireCube(groundCheckPos.position, groundCheckSize);
+		Gizmos.color = Color.blue;
+		Gizmos.DrawWireCube(wallCheckPos.position, wallCheckSize);
 	}
 }
